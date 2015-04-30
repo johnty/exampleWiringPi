f873a85494675926620549689d79691497dd38e7
johnty <johntywang@gmail.com>
2015-04-30 12:23:27 -0400

    analog reading working

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -6,7 +6,24 @@ void ofApp::setup(){
          printf("error init wiringPi\n");
     else
          printf("\n\n\n\nsuccessfully loaded wiringPi!!!!\n");
+    for (int i=0; i<500; i++) {
+         printf("adc test read: %i\n", readADC(0));
+    }
 
+}
+
+int ofApp::readADC(int adcnum) {
+
+    uint8_t buff[3];
+    int adc;
+    if ((adcnum > 7) || (adcnum < 0))
+        return -1;
+    buff[0] = 1;
+    buff[1] = (8+adcnum)<<4;
+    buff[2] = 0;
+    wiringPiSPIDataRW(0, buff, 3);
+    adc = ((buff[1]&3) << 8) + buff[2];
+    return adc;
 
 }
 
@@ -18,6 +35,13 @@ void ofApp::update(){
 //--------------------------------------------------------------
 void ofApp::draw(){
 
+    string output = "ADC values: ";
+    for (int i=0; i<8; i++) {
+        int val = readADC(i);
+        output += ofToString(val)+" ";
+    }
+    ofSetColor(255,0,0);
+    ofDrawBitmapString(output, 100, 100);
 }
 
 //--------------------------------------------------------------
